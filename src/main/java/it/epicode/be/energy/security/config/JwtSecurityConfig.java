f4bfbcf377952a6a664e92fc88058cf81b8f2e37
Daniele Cerulli <dcerulli.dc@gmail.com>
2022-03-16 12:22:20 +0100

    invio 15
    pulito leggermente il codice

--- src/main/java/it/epicode/be/energy/security/config/JwtSecurityConfig.java
+++ src/main/java/it/epicode/be/energy/security/config/JwtSecurityConfig.java
@@ -26,36 +26,6 @@ import it.epicode.be.energy.security.util.AuthTokenFilter;
 @Order(1)
 public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	/*
-	 * @Autowired UserDetailsServiceImpl userDetailsService;
-	 * 
-	 * @Bean public PasswordEncoder passwordEncoder() { return new
-	 * BCryptPasswordEncoder(); }
-	 * 
-	 * @Bean public AuthTokenFilter authenticationJwtTokenFilter() { return new
-	 * AuthTokenFilter(); }
-	 * 
-	 * @Override public void configure(AuthenticationManagerBuilder
-	 * authenticationManagerBuilder) throws Exception {
-	 * authenticationManagerBuilder.userDetailsService(userDetailsService).
-	 * passwordEncoder(this.passwordEncoder()); }
-	 * 
-	 * @Bean public AuthenticationManager authenticationManager() throws Exception {
-	 * return super.authenticationManager(); }
-	 * 
-	 * @Override protected void configure(HttpSecurity http) throws Exception {
-	 * http.headers().frameOptions().sameOrigin().and().csrf().disable().
-	 * authorizeRequests().antMatchers("/auth/**")
-	 * .permitAll().antMatchers("/public/**")
-	 * .permitAll().antMatchers("/api/**").authenticated().and().exceptionHandling()
-	 * .authenticationEntryPoint((req, res, ex) ->
-	 * res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "UNAUTHORIZED : " +
-	 * ex.getMessage()))
-	 * .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.
-	 * STATELESS).and() .addFilterBefore(authenticationJwtTokenFilter(),
-	 * UsernamePasswordAuthenticationFilter.class); }
-	 */
-
 	@Autowired
 	UserDetailsServiceImpl userDetailsService;
 
@@ -81,27 +51,8 @@ public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		/*
-		 * http.headers().frameOptions().sameOrigin().and().csrf().disable().
-		 * authorizeRequests() .antMatchers("/auth/**") .permitAll()
-		 * .antMatchers("/api/**") .authenticated() .antMatchers("/comuni/**")
-		 * .authenticated() .antMatchers("/province/**") .authenticated()
-		 * .antMatchers("/indirizzi/**") .authenticated() .antMatchers("/clienti/**")
-		 * .authenticated() .and() .formLogin() .permitAll() .and()
-		 * 
-		 * 
-		 * // .exceptionHandling() // .authenticationEntryPoint((req, res, ex) ->
-		 * res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "UNAUTHORIZED : " +
-		 * ex.getMessage())) // .and() .sessionManagement()
-		 * //.sessionCreationPolicy(SessionCreationPolicy.STATELESS) .and()
-		 * .addFilterBefore(authenticationJwtTokenFilter(),
-		 * UsernamePasswordAuthenticationFilter.class);
-		 */
-		http.csrf().disable().requestMatchers().antMatchers("/api/**").antMatchers("/auth/**")
-
-				.and().authorizeRequests().antMatchers("/auth/**").permitAll().anyRequest().authenticated().and()
-				.sessionManagement()
-
+		http.csrf().disable().requestMatchers().antMatchers("/api/**").antMatchers("/auth/**").and().authorizeRequests()
+				.antMatchers("/auth/**").permitAll().anyRequest().authenticated().and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
